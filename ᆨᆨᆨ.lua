d2be1921d15b2bb5ce834853a97d2952fcbd522c
blus <blus@iv66.net>
2020-01-31 16:49:35 +0800

    test

---
+++ ᆨᆨᆨ.lua
@@ -0,0 +1,46 @@
+Dvwa
+
+https://www.freebuf.com/articles/web/123779.html
+
+
+110.ninainaideB@gmail.com
+nimainaideB.110
+
+Was 密码
+_ninainaideB
+
+
+Cocos creator 账号密码
+
+nimainaideB110
+nimainaideB.110
+
+python -m SimpleHTTPServer 9999
+
+      lost  -I:9999 
+
+
+      Docker images
+Docker ps -a  所有docker
+docker ps   运行的docker
+docker start id
+Docker stop id
+Docker exec -it id /bin/bash 进入
+docker rm -f id。删除docker
+docker search ubuntu 搜索
+docker pull java。拉去最新的java镜像
+
+https://www.ddosi.com/tag/
+
+
+Md5 解密：
+
+https://www.somd5.com/
+
+*(*F)FI;/M>><>S<?>A:"AS:{[we3[r[1[;dl;/<<><GD>sKELKFLfdsag3456q
+
+3251ca4017e659e8c9a58333db9c6e2a
+
+
+xjf实战；
+https://oj.x10sec.org/?id=2
